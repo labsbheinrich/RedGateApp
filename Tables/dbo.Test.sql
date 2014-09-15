031b4da44a476a6494915f8b009bf28665f6cbb1
bh <bastian.heinrich@lupusalpha.de>
2014-09-15 22:34:14 +0200

    dsafsdf

---
+++ Tables/dbo.Test.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Test]
+(
+[Id] [int] NOT NULL IDENTITY(1, 1),
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[gdsg] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Test] ADD CONSTRAINT [PK_Test] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
+GO
