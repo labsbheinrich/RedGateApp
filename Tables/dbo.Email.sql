f4053c69c2b34fa10a868cdf45a8a3442e4e34b0
labsbheinrich_100 <bastian.heinrich@lupusalpha.de>
2014-09-15 21:38:50 +0200

    chanhe varchatr

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[Email]
 (
 [Id] [int] NOT NULL,
 [email] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[firstname] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[firstname] [varchar] (500) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
